c397c951384bc2b62879a0118e0bf5484457ea33
Ikechukwu001 <michaeljustice727@gmail.com>
2025-08-19 09:01:30 +0100

    Fix: GA3

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,35 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import App from './App'
-import Post from './Post'
-import './index.css' 
+import React, { useEffect } from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
+import App from './App';
+import Post from './Post';
+import './index.css';
+import ReactGA from "react-ga4";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+// Initialize GA once
+ReactGA.initialize("G-KK1XK4L8W4");
+
+// Custom hook for page tracking
+function usePageTracking() {
+  const location = useLocation();
+  useEffect(() => {
+    ReactGA.send({ hitType: "pageview", page: location.pathname });
+  }, [location]);
+}
+
+// Wrapper component so GA can hook into Router changes
+function AnalyticsWrapper() {
+  usePageTracking();
+  return (
     <Routes>
       <Route path="/" element={<App />} />
       <Route path="/post/:slug" element={<Post />} />
     </Routes>
+  );
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
+    <AnalyticsWrapper />
   </BrowserRouter>
-)
+);
---
+++ usePageTracking.js
@@ -0,0 +1,14 @@
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+import ReactGA from "react-ga4";
+
+function usePageTracking() {
+  const location = useLocation();
+
+  useEffect(() => {
+    // Send pageview event on every route change
+    ReactGA.send({ hitType: "pageview", page: location.pathname });
+  }, [location]);
+}
+
+export default usePageTracking;
